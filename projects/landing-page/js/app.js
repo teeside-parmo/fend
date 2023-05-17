1a2dcaa1e7affd3afa13db13884a45d24d206928
teeside-parmo <130171231+teeside-parmo@users.noreply.github.com>
2023-05-17 18:58:12 -0500

    Update app.js

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -119,7 +119,7 @@ const addActiveClass = (conditional, section) => {
 
 // Activating active section
 const sectionActivate = () => {
-    sections.forEach(section => {
+    lpsections.forEach(section => {
         const sectionBalance = balance(section);
 
         inviewport = () => sectionBalance < 150 && sectionBalance >= -150;
