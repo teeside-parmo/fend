94f8f17ce25e190f1826bc882c6946961632d1ba
teeside-parmo <130171231+teeside-parmo@users.noreply.github.com>
2023-05-17 14:21:02 -0500

    Update app.js
    bug fixes

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -96,7 +96,7 @@ function closeMenu() {
 };
 
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+let mybutton = document.getElementById('btn-back-to-top');
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -108,13 +108,13 @@ if (
 document.body.scrollTop > 20 ||
 document.documentElement.scrollTop > 20
 ) {
-mybutton.style.display = "block";
+mybutton.style.display = 'block';
 } else {
-mybutton.style.display = "none";
+mybutton.style.display = 'none';
 }
 }
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+mybutton.addEventListener('click', backToTop);
 
 function backToTop() {
 document.body.scrollTop = 0;
