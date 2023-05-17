a21f13a6fbebbc40ed9217b8e137715b3ee5a04a
teeside-parmo <130171231+teeside-parmo@users.noreply.github.com>
2023-05-17 15:00:52 -0500

    Update app.js

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -95,31 +95,21 @@ function closeMenu() {
     navBarList.classList.remove('active');
 };
 
-//Get the button
-let mybutton = document.getElementById('btn-back-to-top');
+/** Scroll to Top Button  */
+const btn = $('#button');
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-scrollFunction();
-};
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
 
-function scrollFunction() {
-if (
-document.body.scrollTop > 20 ||
-document.documentElement.scrollTop > 20
-) {
-mybutton.style.display = 'block';
-} else {
-mybutton.style.display = 'none';
-}
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener('click', backToTop);
-
-function backToTop() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
-};
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
 
 
 // Add class 'active' to section when near top of viewport
