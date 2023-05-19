fbbdcf44f9d234161032d04f92e1f31e67177ee5
teeside-parmo <130171231+teeside-parmo@users.noreply.github.com>
2023-05-19 12:54:44 -0500

    Update app.js

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -106,14 +106,14 @@ const balance = (section) => {
 // remove active class
 const removeActiveClass = (section) => {
     section.classList.remove('your-active-class');
-    section.style.cssText = "background: linear-gradient(0deg, rgba(139,0,139,1) 0%, rgba(75,0,130,1) 100%);";
+    section.style.cssText = "background:linear-gradient(0deg, rgba(139,0,139,1) 0%, rgba(75,0,130,1) 100%);";
 };
 
 // add active class
 const addActiveClass = (conditional, section) => {
     if(conditional){
         section.classList.add('your-active-class');
-        section.style.cssText = "background: linear-gradient(0deg, rgba(0,132,0,0.5) 0%, rgba(75,0,130,1) 100%);";
+        section.style.cssText = "background: white; color: #000";
     };
 };
 
