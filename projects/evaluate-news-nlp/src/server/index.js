5607df3d4aca517ac4921a0e18d8a2768ca26a05
Dave-Sheridan <shero.dave@gmail.com>
2023-07-18 23:22:23 -0500

    Commit 2

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv');
+//configure env files
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+
 
 const app = express()
 
@@ -10,7 +15,7 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
@@ -21,3 +26,8 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+// You could call it aylienapi, or anything else
+var textapi = new aylien({
+    application_key: process.env.API_KEY
+ });
