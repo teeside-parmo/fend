4aebe3a51fbda167e3f70c43977c1b3cd90bf7f5
teeside-parmo <shero.dave@gmail.com>
2023-08-06 18:29:50 -0500

    Update webpack.dev

--- projects/travel-app/webpack.dev.js
+++ projects/travel-app/webpack.dev.js
@@ -1,48 +1,33 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const GoogleFontsPlugin = require('google-fonts-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  entry: "./src/client/index.js",
   mode: "development",
-  entry: './src/client/index.js',
-  devtool: 'source-map',
-  devServer: {
-    port: 8800
-  },
+  devtool: "source-map",
+  stats: "verbose",
   output: {
-    libraryTarget: 'var',
-    library: 'Client',
-    path: path.resolve(__dirname, 'dist')
+    libraryTarget: "var",
+    library: "Client",
   },
   module: {
     rules: [
       {
-        test: '/\.js$/',
+        test: "/.js$/",
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: "babel-loader",
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
-      {
-        test: /\.html$/,
-        use: ['html-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]'
-            }
-          }
-        ],
-      }
-    ]
+    ],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
   },
   plugins: [
     new HtmlWebPackPlugin({
@@ -55,14 +40,8 @@ module.exports = {
       // Write Logs to Console
       verbose: true,
       // Automatically remove all unused webpack assets on rebuild
-      cleanStaleWebpackAssets: false,
-      protectWebpackAssets: false
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
     }),
-    new GoogleFontsPlugin({
-      fonts: [
-        { family: "Gochi Hand" },
-        { family: "Open Sans" }
-      ]
-    })
-  ]
-}
+  ],
+};
