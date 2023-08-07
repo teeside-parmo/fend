f36e0c3be9c213f9ceebee083fc6a6419943d936
teeside-parmo <shero.dave@gmail.com>
2023-08-06 19:45:49 -0500

    Added API Key

--- projects/travel-app/src/client/js/callPixabayApi.js
+++ projects/travel-app/src/client/js/callPixabayApi.js
@@ -1,5 +1,5 @@
 let pixabayApi = async (cityName) => {
-    const pixabayApiKey = "17157704-eccea26d680203f3390b998d1";
+    const pixabayApiKey = "25587758-04641389b53d35819a02a40fa";
     const pixabayApiReqURL = `https://pixabay.com/api/?key=${pixabayApiKey}&q=${cityName}&image_type=photo`;
     const res = await fetch(pixabayApiReqURL);
     let result = {};
